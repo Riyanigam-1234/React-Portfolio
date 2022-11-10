1f832a3e323a000bca5a95d4331aaaea0527d422
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-11-10 21:14:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Portfolio from './components/portfolio';
+
+function App() {
+  return (
+    <div className="App">
+    <Portfolio/>
+    </div>
+  );
+
+}
+
+export default App;
