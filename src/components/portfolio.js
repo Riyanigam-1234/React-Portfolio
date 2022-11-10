63d2052d422773492b6c0f7393f347b093531f68
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-11-10 21:15:53 +0530

    Update portfolio.js

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,1 +1,58 @@
+import React from 'react'
 
+const Portfolio = () => {
+  return (
+    <div>
+      <div>
+        <h2>Riya Nigam</h2>
+        <h4>Software Developer </h4>
+        <a href="_blank">
+          Linkedin 
+        </a>
+        <a href="_blank">
+           GitHub
+        </a>
+        <hr />
+      </div>
+      <div>
+        <h3>Experience</h3>
+        <h4>Software Developer</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <p>Software Developer</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>DSA</li>
+          <li>React</li>
+        
+        </ul>
+        <hr />
+        <h2>Hobbies</h2>
+        <ol>
+        
+          <li>Reading Books</li>
+          <li>listening music</li>
+          <li>Travelling</li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio;
